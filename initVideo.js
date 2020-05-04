3791cdffdb6c6558baffa29e47393bd743b7c618
Egor Bochkarev <bochkarevei@gmail.com>
2020-05-04 18:30:52 +0300

    Add video controls

--- initVideo.js
+++ initVideo.js
@@ -1,4 +1,34 @@
+const stop = (video) => {
+    video.pause();
+}
+const play = (video) => {
+    video.play();
+}
+
+const expand = (video) => {
+    video.setAttribute('data-mode', 'expanded')
+    document.body.setAttribute('data-mode', 'expanded');
+}
+
+const colapse = (video) => {
+    video.setAttribute('data-mode', 'colapsed')
+    document.body.setAttribute('data-mode', 'colapsed');
+}
+
+
 function initVideo(video, url) {
+    video.addEventListener('playing', () => {
+        video.setAttribute('data-state', 'played')
+    })
+    video.addEventListener('pause', () => {
+        video.setAttribute('data-state', 'paused')
+    });
+    video.addEventListener('click', () => {
+        video.paused ? play(video) : stop(video);
+    });
+    video.addEventListener('dblclick', () => {
+        expand(video);
+    });
     if (Hls.isSupported()) {
         var hls = new Hls();
         hls.loadSource(url);
